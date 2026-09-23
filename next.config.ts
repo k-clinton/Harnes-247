d46d24a8e392da458b09d1d82e36d25d711db7d2
Clinton Omondi <kingclinn40931@gmail.com>
2026-09-23 09:53:40 +0300

    feat: add custom headers configuration for improved security

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
